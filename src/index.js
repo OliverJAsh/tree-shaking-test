1a6755a2c38b1b638dc76b028d01b9989459192a
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-20 13:54:51 +0100

    Named exports

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import * as option from "fp-ts/es6/Option";
 // import { option } from "fp-ts/es6";
 
 console.log(option.fromNullable(1));
+
+// import { fromNullable } from "fp-ts/es6/Option";
+
+// console.log(fromNullable(1));
