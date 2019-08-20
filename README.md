aa89a73d4ba7fc56a7ba41b7e7deed1a359a031e
Oliver Joseph Ash <oliverjash@gmail.com>
2019-08-20 13:31:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+```bash
+webpack
+# Inspect output
+code ./target/index.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const pathHelpers = require("path");
+
+// Expect `__dirname` to be `/config/target/`.
+const ROOT_PATH = __dirname;
+const TARGET_PATH = pathHelpers.join(ROOT_PATH, "./target/");
+const SRC_PATH = pathHelpers.join(ROOT_PATH, "./src/");
+
+const ENTRY_FILENAME = "index.js";
+const OUTPUT_FILENAME = "index.js";
+
+const config = {
+  mode: "production",
+  entry: pathHelpers.resolve(SRC_PATH, ENTRY_FILENAME),
+  output: {
+    path: TARGET_PATH,
+    filename: OUTPUT_FILENAME
+  }
+};
+
+module.exports = config;
