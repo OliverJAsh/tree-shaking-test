f088e56a09f542eaa66eb9988e762e35ae5024a3
Oliver Joseph Ash <oliverjash@gmail.com>
2020-01-15 15:08:16 +0000

    Use production mode and disable unwanted

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,10 @@
 const pathHelpers = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
-// [1] Tree shaking
-
 const config = {
-  // We override the default (`eval-source-map`) so that the output code is not wrapped in eval.
-  devtool: false,
-  mode: "development",
+  mode: "production",
   optimization: {
-    // [1] Step 1: drop references to unused exports, so they can be remove by dead code elimination.
-    usedExports: true,
-    sideEffects: true,
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false,
-
-          compress: {
-            defaults: false,
-
-            // [1] Step 2: dead code elimination.
-            dead_code: true,
-            unused: true
-          }
-        }
-      })
-    ]
+    minimizer: [new TerserPlugin({ terserOptions: { mangle: false } })]
   },
   entry: pathHelpers.join(__dirname, "./src/index.js"),
   output: {
