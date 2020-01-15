e66f2ad9a094103567a9acad33e7e6dc1fb53bf5
Oliver Joseph Ash <oliverjash@gmail.com>
2020-01-15 15:04:07 +0000

    Resolve Node modules in Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 const pathHelpers = require("path");
+const resolve = require('@rollup/plugin-node-resolve')
 
 export default {
   input: pathHelpers.join(__dirname, "./src/index.js"),
   output: {
     file: pathHelpers.join(__dirname, "./target/rollup.js"),
     format: "cjs"
-  }
+  },
+  plugins: [resolve()]
 };
