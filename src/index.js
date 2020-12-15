9002bb50b78a99c26fbb3eca58eef96bde8eaf68
Oliver Joseph Ash <oliverjash@gmail.com>
2020-12-15 09:00:12 +0000

    New

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import { StatusCodes } from "http-status-codes/build/es/status-codes";
+import * as StatusCodes from "http-status-codes/build/es/status-codes";
 console.log(StatusCodes.ACCEPTED);
 console.log(StatusCodes.BAD_GATEWAY);
