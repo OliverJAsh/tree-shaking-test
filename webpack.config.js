6c23d8da3e8a1160b38faf414937e758f92b54da
Oliver Joseph Ash <oliverjash@gmail.com>
2020-12-15 08:55:53 +0000

    Minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,7 @@
 const pathHelpers = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
 
 const config = {
   mode: "production",
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false,
-          output: {
-            beautify: true,
-          },
-        },
-      }),
-    ],
-  },
   entry: pathHelpers.join(__dirname, "./src/index.js"),
   output: {
     path: pathHelpers.join(__dirname, "./target"),
