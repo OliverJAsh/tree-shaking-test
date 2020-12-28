f67a63636519fd992a1aacb33d6c38cfc9daa64d
Oliver Joseph Ash <oliverjash@gmail.com>
2020-12-28 12:05:15 +0000

    Toggle

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,27 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 const config = {
   mode: "production",
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: false,
-          output: {
-            beautify: true,
-          },
-        },
-      }),
-    ],
-  },
+
+  // optimization: {
+  //   minimizer: [
+  //     new TerserPlugin({
+  //       terserOptions: {
+  //         mangle: false,
+  //         output: {
+  //           beautify: true,
+  //         },
+  //       },
+  //     }),
+  //   ],
+  // },
+
+  // mode: "development",
+  // optimization: {
+  //   usedExports: true,
+  // },
+  // // Prevent eval(…)
+  // devtool: "source-map",
+
   entry: pathHelpers.join(__dirname, "./src/index.js"),
   output: {
     path: pathHelpers.join(__dirname, "./target"),
